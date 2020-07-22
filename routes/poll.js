ac74fc24768c235c32e021038d401dc8e18a0e52
Saiyedul Bashar <65618180+saiyedulbas@users.noreply.github.com>
2020-07-22 18:49:41 +0600

    Update poll.js

--- routes/poll.js
+++ routes/poll.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
 
-const Vote = require('../models/Vote');
-const Pusher = require('pusher');
-const keys = require('../config/keys');
-var pusher = new Pusher({
-  appId: '1020030',
   key: 'd26e913ee5b2f3dc94ba',
   secret: 'a8d1edd2aacf9394ad8d',
-  cluster: 'mt1',
-  encrypted: true
-});
-router.get('/', (req, res) => {
-  Vote.find().then(votes => res.json({ success: true, votes: votes }));
-});
+  cluster: 'mt1',const express = require('express');
+          const router = express.Router();
+          const mongoose = require('mongoose');
 
-router.post('/', (req, res) => {
-  const newVote = {
-    os: req.body.os,
-    points: 1
+          const Vote = require('../models/Vote');
+          const Pusher = require('pusher');
+          const keys = require('../config/keys');
+                  
   };
   
-  new Vote(newVote).save().then(vote => {
-    pusher.trigger('os-poll', 'os-vote', {
-      points: parseInt(vote.points),
-      os: vote.os
-    });
+  new Vote(newVote).save()var pusher = new Pusher({
+          appId: '1020030',
+          encrypted: true
+        });
+        router.get('/', (req, res) => {
+          Vote.find().then(votes => res.json({ success: true, votes: votes }));
+        });
 
-    return res.json({ success: true, message: 'Thank you for voting' });
-  });
-});
+        router.post('/', (req, res) => {
+          const newVote pusher.trigger('os-poll', 'os-vote', {
+              points: parseInt(vote.points),
+              os: vote.os
+            });
 
-module.exports = router;
+            return res.json({ success: true, message: 'Thank you for voting' });
+          });
+        });
+
+        module.exports = router;= {
+            os: req.body.os,
+            points: 1.then(vote => {
+            
