67ccaeac151b40aba5e8915b9f8d1f2a69780dba
Saiyedul Bashar <65618180+saiyedulbas@users.noreply.github.com>
2020-07-22 18:51:31 +0600

    Update Vote.js

--- models/Vote.js
+++ models/Vote.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const VoteSchema = new Schema({
-  os: {
-    type: String,
-    required: true
-  },
-  points: {
-    type: String,
-    required: true
-  }
-});
+        type: String,
+        required: trueconst mongoose = require('mongoose');
+    const Schema = mongoose.Schema;
 
-// Create collection and add schema
-const Vote = mongoose.model('Vote', VoteSchema);
+    const VoteSchema = new Schema({
+      os: {required: true
+                      },
+                  points: {
+                  }
+        type: String,
+        
+                                });
 
-module.exports = Vote;
+                // Create collection and add schema
+                const Vote = mongoose.model('Vote', VoteSchema);
+
+                module.exports = Vote;
